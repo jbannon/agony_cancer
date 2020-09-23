9a5878bc3e60a2fe6b33f46b085a9a833a5e4ff1
jb <jjb509@nyu.edu>
2020-09-23 12:40:06 -0400

    oopsie

--- src/agony_pipeline.py
+++ src/agony_pipeline.py
@@ -1,7 +1,24 @@
 import os
+import sys
+
+def replace_rename(ifname, ofname):
+	with open(ifname,"r") as f:
+		lines = f.readlines()
+	for i in range(len(lines)):
+		lines[i]=lines[i].replace(","," ")
+	with open(ofname,"w") as of:
+		of.writelines(lines)
 
 
+def main():
+	os.system("gcc -Wall pphi_bs.c -fopenmp -o phixer_.out")
+	os.system("./phixer.out test_100_585.txt")
+	replace_rename("pruned_nw_bs10_bins12_test_100_585.txt","pruned_nw_bs10_bins12_test_100_585.txt")
 
-os.system("gcc -Wall pphi_bs.c -fopenmp -o phixer_.out")
-os.system("./phixer.out test_100_585.txt")
-os.system("agony_weighted/agony -i pruned_nw_bs10_bins12_test_100_585.txt -o res.txt -w")
+	os.system("agony_weighted/agony -i pruned_nw_bs10_bins12_test_100_585.txt -o res.txt -w")
+
+if __name__=='__main__':
+	iname = sys.argv[1]
+	oname =sys.argv[2]
+	main()
+	
