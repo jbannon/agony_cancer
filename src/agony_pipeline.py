a5c09b46aff7d0eb45324ffab217d63d09344138
jb <jjb509@nyu.edu>
2020-09-23 10:15:11 -0400

    fixed pipeline, need to make remotely

--- src/agony_pipeline.py
+++ src/agony_pipeline.py
@@ -4,4 +4,4 @@ import os
 
 os.system("gcc -Wall pphi_bs.c -fopenmp -o phixer_.out")
 os.system("./phixer.out test_100_585.txt")
-os.system("agony_weighted/agony -i pruned*.txt")
+os.system("agony_weighted/agony -i pruned_nw_bs10_bins12_test_100_585.txt -o res.txt -w")
